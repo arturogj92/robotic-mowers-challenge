78ac94bc0fe40a73f607545467758115474a42b8
0xAuron <arturogarciaasir@gmail.com>
2022-01-29 01:22:26 +0100

    initial commit

---
+++ src/main/kotlin/com/agjurado/roboticmowers/RoboticMowerChallengeApplication.kt
@@ -0,0 +1,11 @@
+package com.agjurado.roboticmowers
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoboticMowerChallengeApplication
+
+fun main(args: Array<String>) {
+    runApplication<RoboticMowerChallengeApplication>(*args)
+}
---
+++ src/test/kotlin/com/agjurado/roboticmowers/RoboticMowerChallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.agjurado.roboticmowers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RoboticMowerChallengeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
