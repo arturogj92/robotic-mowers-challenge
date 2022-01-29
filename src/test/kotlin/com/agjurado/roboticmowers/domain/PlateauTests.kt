374ca6be523acbff4c7442a01bfac0c08564d68e
0xAuron <arturogarciaasir@gmail.com>
2022-01-29 16:37:30 +0100

    testing creating an invalid Plateau without factory method

--- src/test/kotlin/com/agjurado/roboticmowers/domain/PlateauTests.kt
+++ src/test/kotlin/com/agjurado/roboticmowers/domain/PlateauTests.kt
@@ -5,21 +5,29 @@ import com.agjurado.roboticmowers.domain.error.PlateauError
 import io.kotest.assertions.arrow.either.shouldBeLeft
 import io.kotest.assertions.arrow.either.shouldBeRight
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class PlateauTests {
 
     @Test
     fun `a plateau can be created if the upper right coords are correct`() {
-        val plateau: Either<PlateauError, Plateau> = Plateau.createPlateauFrom(upperRightCoords = Coords(5,5))
+        val validPlateau: Either<PlateauError, Plateau> = Plateau.createPlateauFrom(upperRightCoords = Coords(5,5))
 
-        plateau.shouldBeRight()
+        validPlateau.shouldBeRight()
     }
 
     @Test
     fun `a plateau can't be created if the upper right coords are invalid`() {
-        val plateau: Either<PlateauError, Plateau> = Plateau.createPlateauFrom(upperRightCoords = Coords(-1,5))
+        val invalidPlateau: Either<PlateauError, Plateau> = Plateau.createPlateauFrom(upperRightCoords = Coords(-1,5))
 
-        plateau.shouldBeLeft()
+        invalidPlateau.shouldBeLeft()
+    }
+
+    @Test
+    fun `if the plateau is created without using the factory method and is invalid, an exception is raised`() {
+        assertThrows<IllegalArgumentException> {
+            Plateau(upperRightCoords = Coords(-1, -1))
+        }
     }
 
 
